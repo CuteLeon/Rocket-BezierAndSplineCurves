b20d31f353b12ca3e844cb48a003ce380777ba4d
Leon <xiaoyankeji@hotmail.com>
2016-08-01 14:34:24 +0800

    第一次提交

---
+++ 飞行的小火箭/BezierAndSplineCurves.Designer.vb
@@ -0,0 +1,139 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class BezierAndSplineCurves
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.TheRocket = New System.Windows.Forms.Label()
+        Me.RocketEngine = New System.Windows.Forms.Timer(Me.components)
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TheRocket
+        '
+        Me.TheRocket.BackColor = System.Drawing.Color.Transparent
+        Me.TheRocket.ForeColor = System.Drawing.Color.Yellow
+        Me.TheRocket.Location = New System.Drawing.Point(162, 218)
+        Me.TheRocket.Name = "TheRocket"
+        Me.TheRocket.Size = New System.Drawing.Size(64, 64)
+        Me.TheRocket.TabIndex = 0
+        '
+        'RocketEngine
+        '
+        Me.RocketEngine.Interval = 50
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.BackColor = System.Drawing.Color.Transparent
+        Me.CheckBox1.ForeColor = System.Drawing.Color.White
+        Me.CheckBox1.Location = New System.Drawing.Point(12, 12)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(96, 16)
+        Me.CheckBox1.TabIndex = 1
+        Me.CheckBox1.Text = "显示路径曲线"
+        Me.CheckBox1.UseVisualStyleBackColor = False
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.BackColor = System.Drawing.Color.Transparent
+        Me.CheckBox2.ForeColor = System.Drawing.Color.White
+        Me.CheckBox2.Location = New System.Drawing.Point(12, 34)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(96, 16)
+        Me.CheckBox2.TabIndex = 2
+        Me.CheckBox2.Text = "显示关键坐标"
+        Me.CheckBox2.UseVisualStyleBackColor = False
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.BackColor = System.Drawing.Color.Transparent
+        Me.CheckBox3.ForeColor = System.Drawing.Color.White
+        Me.CheckBox3.Location = New System.Drawing.Point(12, 56)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(96, 16)
+        Me.CheckBox3.TabIndex = 3
+        Me.CheckBox3.Text = "显示路径坐标"
+        Me.CheckBox3.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("微软雅黑", 10.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(8, 83)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 80)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "坐标：" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "角度：" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "速度：" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "进度："
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("微软雅黑", 10.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(57, 83)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 20)
+        Me.Label2.TabIndex = 5
+        '
+        'BezierAndSplineCurves
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(70, Byte), Integer))
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ClientSize = New System.Drawing.Size(800, 500)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.TheRocket)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.DoubleBuffered = True
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "BezierAndSplineCurves"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "三次样条曲线和贝塞尔曲线（鼠标点击坐标可拖动）"
+        Me.TopMost = True
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TheRocket As Label
+    Friend WithEvents RocketEngine As Timer
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ 飞行的小火箭/BezierAndSplineCurves.vb
@@ -0,0 +1,124 @@
+﻿Imports System.Drawing.Drawing2D
+
+Public Class BezierAndSplineCurves
+    '创建路径使用的坐标个数
+    Private Const PointBound As Integer = 10
+    Private Const LastPointsCount As Integer = 4
+    '绘图显示的数据(依次为：路径曲线、关键坐标、路径坐标)
+    Dim ShowPath As Boolean = False
+    Dim ShowCurvedPoints As Boolean = False
+    Dim ShowPathPoints As Boolean = False
+
+    Dim CurvedPoints(PointBound) As PointF      '关键坐标
+    Dim PathPoints As PointF()                            '路径坐标
+    Dim PathPointIndex As Integer = 0               '当前路径坐标标识
+    Dim LastPoint As PointF = New PointF(0, 0)  '前一坐标(用于计算角度)
+    Dim NextPoint As PointF                               '后一坐标(用于计算角度)
+    Dim Angle As Single                                      '火箭旋转角度
+    Dim RocketBitmap As Bitmap                        '火箭位图
+
+#Region "窗体"
+
+    Private Sub BezierAndSplineCurves_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        NewPathAndPoints()
+        RocketEngine.Start()
+    End Sub
+
+    Private Sub RocketEngine_Tick(sender As Object, e As EventArgs) Handles RocketEngine.Tick
+        LastPoint = PathPoints(PathPointIndex)
+        PathPointIndex += 1
+        If PathPointIndex = UBound(PathPoints) Then NewPathAndPoints()
+
+        NextPoint = PathPoints(PathPointIndex + 1)
+        Angle = Math.Atan2(LastPoint.Y - NextPoint.Y, NextPoint.X - LastPoint.X)
+        Angle = -Angle / (2 * Math.PI) * 360
+        RocketBitmap = GetRotateBitmap(My.Resources.RocketResource.Rocket, Angle)
+        TheRocket.Image = RocketBitmap
+
+        TheRocket.Location = New Point(PathPoints(PathPointIndex).X - 32, PathPoints(PathPointIndex).Y - 32)
+
+        Label2.Text = PathPoints(PathPointIndex).ToString & vbCrLf & Angle.ToString("###") & vbCrLf & Math.Sqrt((LastPoint.X - PathPoints(PathPointIndex).X) ^ 2 + (LastPoint.Y - PathPoints(PathPointIndex).Y) ^ 2).ToString("###") & vbCrLf & PathPointIndex & " / " & PathPoints.Count
+
+        DrawImage()
+    End Sub
+
+#End Region
+
+#Region "功能函数"
+    Private Sub NewPathAndPoints()
+        Dim Index As Integer
+        For Index = 1 To LastPointsCount
+            CurvedPoints(Index - 1) = CurvedPoints(PointBound - LastPointsCount + Index)
+        Next
+        For Index = LastPointsCount To PointBound
+            CurvedPoints(Index) = New PointF(VBMath.Rnd * Me.Width, VBMath.Rnd * Me.Height)
+        Next
+
+        PathPoints = GetCurvedPoints(CurvedPoints, PointBound - 3)
+        PathPointIndex = 0
+    End Sub
+
+    Private Function GetCurvedPoints(ByVal DrawPoints As PointF(), ByVal NumberOfSegments As Integer) As PointF()
+        Dim MyGraphicsPath As GraphicsPath = New GraphicsPath
+        Dim MyPoints As PointF()
+        MyGraphicsPath.AddCurve(DrawPoints, 2, NumberOfSegments, 0.5)
+        MyGraphicsPath.Flatten()
+        MyPoints = MyGraphicsPath.PathPoints
+        MyGraphicsPath.Dispose()
+        Return MyPoints
+    End Function
+
+    Private Function GetRotateBitmap(ByVal BitmapRes As Bitmap, ByVal Angle As Single) As Bitmap
+        Dim ReturnBitmap As New Bitmap(BitmapRes.Width, BitmapRes.Height)
+        Dim MyGraphics As Graphics = Graphics.FromImage(ReturnBitmap)
+        MyGraphics.SmoothingMode = SmoothingMode.HighQuality
+        MyGraphics.PixelOffsetMode = PixelOffsetMode.HighQuality
+        MyGraphics.TranslateTransform(BitmapRes.Width / 2, BitmapRes.Height / 2)
+        MyGraphics.RotateTransform(Angle, MatrixOrder.Prepend)
+        MyGraphics.TranslateTransform(-BitmapRes.Width / 2, -BitmapRes.Height / 2)
+        MyGraphics.DrawImage(BitmapRes, 0, 0, BitmapRes.Width, BitmapRes.Height)
+        MyGraphics.Dispose()
+        Return ReturnBitmap
+    End Function
+
+    Private Sub DrawImage()
+        If Not (ShowCurvedPoints Or ShowPath Or ShowPathPoints) Then Me.BackgroundImage = Nothing : Exit Sub
+
+        Dim MyBitmap As Bitmap = New Bitmap(Me.Width, Me.Height)
+        Dim MyGraphics As Graphics = Graphics.FromImage(MyBitmap)
+        MyGraphics.SmoothingMode = SmoothingMode.HighQuality
+        MyGraphics.PixelOffsetMode = PixelOffsetMode.HighQuality
+
+        If ShowPath Then MyGraphics.DrawCurve(New Pen(Color.Red, 2), CurvedPoints)
+
+        If ShowPathPoints Then
+            For Each a As PointF In PathPoints
+                MyGraphics.FillEllipse(Brushes.Pink, New RectangleF(a.X, a.Y, 5, 5))
+            Next
+        End If
+
+        If ShowCurvedPoints Then
+            For Index As Integer = LBound(CurvedPoints) To UBound(CurvedPoints)
+                MyGraphics.DrawEllipse(New Pen(Color.Yellow, 3), New Rectangle(CurvedPoints(Index).X - 2, CurvedPoints(Index).Y - 2, 4, 4))
+                MyGraphics.DrawString(Index.ToString, Me.Font, Brushes.Yellow, CurvedPoints(Index).X + 2, CurvedPoints(Index).Y + 2)
+            Next
+        End If
+
+        Me.BackgroundImage = MyBitmap
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        ShowPath = CheckBox1.Checked
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        ShowCurvedPoints = CheckBox2.Checked
+    End Sub
+
+    Private Sub CheckBox3_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox3.CheckedChanged
+        ShowPathPoints = CheckBox3.Checked
+    End Sub
+
+#End Region
+
+End Class
